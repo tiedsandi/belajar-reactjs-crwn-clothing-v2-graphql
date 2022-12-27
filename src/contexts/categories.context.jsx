ae7013a5a50afd0c7d5b0f3266fdcfcd9e1a2d11
tiedSandi <wkwkwkand@gmail.com>
2022-12-27 17:06:35 +0700

    using graphql in crwn clothing v2

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+const COLLECTIONS = gql`
+  query GetCollections {
+    collections {
+       id
+      title
+      items{
+        id
+        name
+        price
+        imageUrl
+      } 
+    }
+  }
+`
+
+export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS)
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    if (data) {
+      const { collections } = data
+      const collectionMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {})
+      setCategoriesMap(collectionMap)
+    }
+  }, [data])
+
+  const value = { categoriesMap, loading };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
